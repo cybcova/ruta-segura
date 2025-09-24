4bf070476373a2d78a8cf0393f7723820a3ebbae
CMD-UAEH <co540565@uaeh.edu.mx>
2025-09-24 00:39:40 -0600

    Update Test.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function App() {
+function Test() {
   const [status, setStatus] = useState("idle");
 
   const send = async () => {
@@ -38,4 +38,4 @@ function App() {
   );
 }
 
-export default App;
+export default Test;
