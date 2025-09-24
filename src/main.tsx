f6447811ddd7846c0416ab2921bef9642b9e267f
CMD-UAEH <co540565@uaeh.edu.mx>
2025-09-24 00:50:51 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Test from "./Test";
 import "./index.css";
 
-const container = document.getElementById("root");
-if (!container) {
-  throw new Error('Root element "#root" not found. Asegúrate de tener <div id="root"></div> en index.html');
-}
+const container = document.getElementById("root") as HTMLElement;
 
 createRoot(container).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/test" element={<Test />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
